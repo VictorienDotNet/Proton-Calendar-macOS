3b70f65e9ef2c8cd8ac46dd5db263908efbc9a1d
Victorien <5654077+VictorienDotNet@users.noreply.github.com>
2026-05-28 14:07:51 +0200

    Update the initilURL

--- Sources/CalendarRootView.swift
+++ Sources/CalendarRootView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct CalendarRootView: View {
     var body: some View {
         ProtonWebView(
-            initialURL: URL(string: "https://account.proton.me/calendar")!
+            initialURL: URL(string: "https://calendar.proton.me/")!
+           
+            //https://account.proton.me/calendar
         )
         .background(WindowTitleHider())
         .ignoresSafeArea()
